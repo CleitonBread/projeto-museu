5a8126a93cfc3f0b4f1ec88c32a2f11e850a66e3
Cleiton <cleitonserafim13@gmail.com>
2025-10-30 23:17:34 -0300

    adicionando o carrossel-imagens javascript

---
+++ src/js/carrossel-imagens.js
@@ -0,0 +1,48 @@
+//Pegando todas as imagens dos carrosseis de imagens
+let imagensPainel = document.querySelectorAll(".imagem-painel");
+
+//pegando as setas de Avançar e voltar
+const setaAvancar = document.getElementById("btn-avancar");
+const setaVoltar = document.getElementById("btn-voltar");
+
+//indice das imagens utilizado nas funções
+let imagemIndice = 0;
+
+//funções:
+
+function esconderImagens(){
+    imagensPainel.forEach(imagem =>{
+        imagem.classList.remove("mostrar");
+    })
+}
+
+function mostrarImagens(){
+    imagensPainel[imagemIndice].classList.add("mostrar");
+}
+
+setaAvancar.addEventListener("click", function(){
+    setaVoltar.classList.remove("final-imagens");
+
+    const totalDeImagens = imagensPainel.length -1;
+    if(imagemIndice === totalDeImagens){
+        setaAvancar.classList.add("final-imagens");
+        return;
+    }
+
+    imagemIndice++;
+    esconderImagens();
+    mostrarImagens();
+})
+
+setaVoltar.addEventListener("click", function(){
+    setaAvancar.classList.remove("final-imagens");
+
+    if(imagemIndice === 0){
+        setaVoltar.classList.add("final-imagens");
+        return;
+    }
+
+    imagemIndice--;
+    esconderImagens();
+    mostrarImagens();
+})
